9d9ed4a925f891646c4cb193281dc57e01c4ce46
mfelixss <mateusffelix.ss@gmail.com>
2020-10-04 23:26:26 -0300

    teste03

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/index', indexControle.index)
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log("Listening on port " + PORT)
+})
